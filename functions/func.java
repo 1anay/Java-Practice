c072d0e5324cd6866f6b6b2976f6f501c63b6050
Anay dhyani <73874512+1anay@users.noreply.github.com>
2023-02-09 13:51:12 +0530

    Add files via upload

---
+++ functions/func.java
@@ -0,0 +1,15 @@
+class func
+{
+    public static boolean prime(int n) {
+        if (n % 2 == 0)
+            return true;
+        else
+            return false;
+    }
+    public static void main (String[] args){
+        int n= 5;
+        System.out.println(prime(n));
+
+        }
+
+}
