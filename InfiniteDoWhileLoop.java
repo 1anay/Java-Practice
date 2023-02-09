15745788141a94bd439e994fe6fbdf2ce9b4293f
Anay dhyani <73874512+1anay@users.noreply.github.com>
2023-02-09 17:13:28 +0530

    Add files via upload

---
+++ InfiniteDoWhileLoop.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class InfiniteDoWhileLoop {
+    public static void main (String[] args){
+        Scanner scr=new Scanner(System.in);
+        int count=0;
+        int n=18;
+        do {
+            System.out.print(count);
+        }while(n==18);
+        }
+
+}
